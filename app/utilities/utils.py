d8ae20d6f743e0ce7cdb6e0baf510e5763c18248
Nuno Ribeiro <nribeiro@semtech.com>
2022-07-06 19:06:49 +0200

    formatting

--- app/utilities/utils.py
+++ app/utilities/utils.py
@@ -5,8 +5,9 @@ def print_json(data):
     s = json.dumps(data, indent=4, sort_keys=True)
     print(s)
 
+
 def byte_to_json(my_bytes_value, replace=True):
-    # Decode UTF-8 bytes to Unicode, and convert single quotes 
+    # Decode UTF-8 bytes to Unicode, and convert single quotes
     # to double quotes to make it valid JSON
     if replace:
         my_json = my_bytes_value.decode('utf8').replace("'", '"')
@@ -17,4 +18,3 @@ def byte_to_json(my_bytes_value, replace=True):
 
     # Load the JSON to a Python list & dump it back out as formatted JSON
     return json.loads(my_json)
-    
